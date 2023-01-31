0f6ed49f07785b0c6b15f1a9b308ed86d2e00942
asharibali <>
2023-01-31 23:27:01 +0500

    intial commit

--- todos.js
+++ todos.js
@@ -1,9 +1,9 @@
-const emojis = ['😃', '😎', '😍', '😜', '😂', '🔥'];
+const emojis = ['😃', '😎', '😍', '😜', '🔥'];
 
 export const todos = [
-  { id: 1, text: 'Write code for todo list app', completed: true },
-  { id: 2, text: 'Test the app', completed: false },
-  { id: 3, text: 'Deploy the app', completed: false }
+  { id: 1, text: 'Learn Blockchain Development', completed: false },
+  { id: 2, text: 'Panaverse Assignments Completed', completed: false },
+  { id: 3, text: 'Building dapps with Next.js 13', completed: false }
 ];
 
 export function addTodo(text) {
